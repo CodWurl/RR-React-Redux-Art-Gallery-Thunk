0c6b9048109e0165eb18896d2863dae712f0258c
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-08-30 20:39:49 -0400

    changes to js files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
+import {fetchData} from './features/dataSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const currentState = useSelector (state => state.data)
+  const renderImage = () => {
+   return currentState.apiData.primaryImage ?
+  <img src = {currentState.apiData.primaryImage}/>:
+  <h3>No Image Found</h3>
+  }
   // your logic goes here!
 
   return (
     <div className="App">
       <div>
-        <button onClick={() => {}}>Trigger Thunk</button>
+        <button onClick={() => {dispatch(fetchData())}}>Trigger Thunk</button>
         <button onClick={() => {}}>Clear</button>
         <button onClick={() => {}}>Next</button>
         <button onClick={() => {}}>Back</button>
       </div>
+      <h1>{currentState.artId}</h1>
       <input onChange={(e) => { }} />
       <div>
-        {/* Once you have plugged everything in, render the image here! */}
+        {
+          renderImage()
+        }
       </div>
     </div>
   );
