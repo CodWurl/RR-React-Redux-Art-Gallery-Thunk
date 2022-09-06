312426e0c8bc072bc21c0e66b629c0a9444a23ab
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-09-06 11:41:29 -0400

    add changes

--- src/features/middleware.js
+++ src/features/middleware.js
@@ -2,7 +2,7 @@ export const logger = store => next => action => {
     if (typeof action === 'function') {
         action(store.dispatch, store.getState);
     } else { 
-        console.log('dispatch', store.getState())
+        console.log('before dispatch', store.getState())
         next(action)
         console.log('after dispatch', store.getState())
     }
